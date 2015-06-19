7a45f02bc7896fcfe1421e276db5f099ae3cbd02
Yan Ma <mayanworkmail@gmail.com>
2015-06-19 18:12:40 -0400

    First Bad Version #74 --- accepted

---
+++ src/edu/pitt/binarysearch/VersionControl.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package edu.pitt.binarysearch;
+
+/**
+ * VersionControl #74
+ * @author yanma
+ *
+ */
+public class VersionControl {
+	public static boolean isBadVersion(int k) {
+		return true;
+	}
+}
